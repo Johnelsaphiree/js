cdf2f3e60939ff35c923068f0c0b55038e1399ae
Johnelsaphiree <johnelsaphire@gmail.com>
2023-10-22 01:34:43 +0100

    added task 3

---
+++ challenge_3.js
@@ -0,0 +1,11 @@
+/*
+what does  'resultVariable' print
+*/
+
+const variable1 = 20;
+
+const variable2 = 40;
+
+const resultvariable = variable1 + variable2;
+
+console.log(resultvariable); // the answer is 60, which is operator data type.
