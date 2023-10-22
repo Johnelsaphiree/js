15a7c4e62c9f983db48d5f1ccda0009ae4e07b00
Johnelsaphiree <johnelsaphire@gmail.com>
2023-10-22 01:24:26 +0100

    solution 2 solved

---
+++ challenge_2.js
@@ -0,0 +1,8 @@
+//there are three things wrong with this code. find them and fix them.
+const some Number = 20; //the const here is the first mistake
+
+someNumber = 50; // the second mistake is that the variable was declared with a space and the js does'nt recognises it
+//solution
+
+let someNumber = 20; // declared a varaible with space
+someNUmber = 50; // noticed i used lete because the variable will be rea-assigned
